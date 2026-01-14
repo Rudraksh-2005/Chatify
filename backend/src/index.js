2710f328e83ba243ed70363b8327cbc5a807878b
Rudraksh-200 <rudrakshcode58@gmail.comgit config --global user.email rudrakshcode58@gmail.com>
2026-01-14 17:01:48 +0530

    initial commmit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,27 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
-
+import path from 'path';
 
 dotenv.config();
 const PORT=process.env.PORT||3000;
 const app=express();
 
+const __dirname = path.resolve();
+
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
+//make ready for deployment
+console.log("NODE_ENV =", process.env.NODE_ENV);
+
+if(process.env.NODE_ENV=== "production"){
+    app.use(express.static(path.join(__dirname,'../../frontend/dist')));
+    app.get(/.*/,(_,res)=>{
+        res.sendFile(path.join(__dirname,'../../frontend','dist','index.html'));
+    });
+}
 
 app.listen(PORT,()=>{
     console.log('Server is running on port 3000 ');
